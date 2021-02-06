ad9556472be66228bcfce37f73e03f9d3ef89502
Kefan Dong <k27dong@uwaterloo.ca>
2021-02-06 18:56:29 -0500

    added helper functions

---
+++ helper.js
@@ -0,0 +1,62 @@
+exports.help_array = [
+  "```\
+Johnny's in the basement\n\
+Mixing up the medicine\n\
+I'm on the pavement\n\
+Thinking about the government```",
+  "```\
+The man in the trench coat\n\
+Badge out, laid off\n\
+Says he's got a bad cough\n\
+ Wants to get it paid off```",
+  "```\
+A man in the coonskin cap\n\
+In the pig pen\n\
+Wants eleven dollar bills\n\
+You only got ten```",
+  "```\
+The phone's tapped anyway\n\
+Maggie says that many say\n\
+They must bust in early May\n\
+Orders from the D.A.```",
+  "```\
+Walk on your tip toes\n\
+Don't tie no bows\n\
+Better stay away from those\n\
+That carry around a fire hose```",
+  "```\
+Keep a clean nose\n\
+Watch the plain clothes\n\
+You don't need a weather man\n\
+To know which way the wind blows```",
+  "```\
+Try hard, get barred\n\
+Get back, write braille\n\
+Get jailed, jump bail\n\
+Join the army if you fail```",
+  "```\
+Girl by the whirlpool\n\
+Lookin' for a new fool\n\
+Don't follow leaders\n\
+Watch the parkin' meters```",
+  "```\
+Please her, please him, buy gifts\n\
+Don't steal, don't lift\n\
+Twenty years of schooling\n\
+And they put you on the day shift```",
+  "```\
+Better jump down a manhole\n\
+Light yourself a candle\n\
+Don't wear sandals\n\
+Try to avoid the scandals```",
+]
+
+exports.general_msg = () => {
+  const msg = ```command list:\n\
+set_user [id]: set current user\n\
+current_user: display info about current user\n\
+show_playlist: show current user's playlist\n\
+playlist_info [index]\n\
+```
+  return msg
+}
---
+++ util.js
@@ -0,0 +1,40 @@
+exports.shuffle = (arr) => {
+  var curr_index = arr.length, temp, ran_index;
+
+  // While there remain elements to shuffle...
+  while (0 !== curr_index) {
+    ran_index = Math.floor(Math.random() * curr_index);
+    curr_index -= 1;
+
+    // And swap it with the current element.
+    temp = arr[curr_index];
+    arr[curr_index] = arr[ran_index];
+    arr[ran_index] = temp;
+  }
+
+  return arr;
+}
+
+exports.parse_lrc = (lrc) => {
+  let sanitized = lrc.split("\n")
+  for (let i = 0; i < sanitized.length; i++) {
+    let l = sanitized[i]
+    try {
+      if (l.length >= 0 && l[0] == '[') {
+        sanitized[i] = sanitized[i].slice(sanitized[i].indexOf(']') + 1)
+      }
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  let prased = "```"
+  for (let i = 0; i < sanitized.length; i++) {
+    if (prased.length < 1990) {
+      prased += sanitized[i] + "\n"
+    }
+  }
+  prased += "```"
+  return prased
+}
