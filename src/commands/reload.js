bfe9c47661e614db4856ba5df2c0bcb6b4811b31
Kefan Dong <k27dong@uwaterloo.ca>
2021-03-20 11:24:38 -0400

    style fix

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -6,18 +6,16 @@ exports.run = (client, message, args) => {
   else {
     const command_name = args[0]
 
-    // Check if the command exists and is valid
     if (!client.commands.has(command_name)) {
       return message.reply(`${command_name} is not a valid command`)
     }
 
-    // the path is relative to the *current folder*, so just ./filename.js
     delete require.cache[require.resolve(`./${command_name}.js`)]
 
-    // We also need to delete and reload the command from the client.commands Enmap
     client.commands.delete(command_name)
     const props = require(`./${command_name}.js`)
     client.commands.set(command_name, props)
+
     message.reply(`${command_name} has been reloaded`)
   }
 }
