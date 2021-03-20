91f02e11096207a39c5159be9159634303deb33f
Kefan Dong <k27dong@uwaterloo.ca>
2021-03-20 11:26:46 -0400

    add pause command

---
+++ src/commands/pause.js
@@ -0,0 +1,21 @@
+const { assert_queue } = require("../helper")
+
+module.exports = {
+  info: {
+    name: "pause",
+  },
+
+  run: async (client, message, args) => {
+    try {
+      let queue = assert_queue(message)
+
+      queue.playing = false
+      queue.connection.dispatcher.pause()
+
+      message.channel.send("Ozy paused!")
+    } catch (err) {
+      console.error(err)
+      message.channel.send(`Error (${this.info.name}): ${err}`)
+    }
+  },
+}
