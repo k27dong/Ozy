48f86997789c6d80debcc453b684f7ed1559bbff
Kefan Dong <k27dong@uwaterloo.ca>
2021-03-23 00:36:42 -0400

    add playlist

---
+++ src/commands/playlist.js
@@ -0,0 +1,72 @@
+const { assert_queue, parse_playlist_list } = require("../helper")
+const { get_user_playlist, get_songs_from_playlist } = require("../api")
+const { play } = require("../player")
+
+module.exports = {
+  info: {
+    name: "playlist",
+  },
+
+  run: async (client, message, args) => {
+    const reply_filter = (m, message) => {
+      return m.author.id === message.author.id
+    }
+
+    try {
+      let queue = assert_queue(message)
+      if (!queue.user) {
+        message.channel.send(`No user set!`)
+      } else {
+        let playlist = await get_user_playlist(queue.user.userId)
+        message.channel.send(parse_playlist_list(playlist)).then(async () => {
+          message.channel
+            .awaitMessages((m) => reply_filter(m, message), {
+              max: 1,
+              time: 30000,
+              errors: ["time"],
+            })
+            .then(async (collected) => {
+              let selection = Number(collected.array()[0].content)
+
+              if (
+                isNaN(selection) ||
+                !Number.isInteger(selection) ||
+                selection <= 0 ||
+                selection > playlist.length
+              ) {
+                message.channel.send(`${selection} is not a valid selection`)
+              }
+
+              let selected_playlist = playlist[selection - 1]
+              let songs = await get_songs_from_playlist(selected_playlist)
+
+              if (songs.length > 0) {
+                for (let song of songs) {
+                  queue.track.push(song)
+                }
+                message.channel.send(
+                  `**Queued** ${songs.length} song${
+                    songs.length > 1 ? "s" : ""
+                  } from ${selected_playlist.name}`
+                )
+
+                if (!queue.playing) {
+                  queue.playing = true
+                  queue.curr_pos = queue.track.length - songs.length
+                  play(message)
+                }
+              } else {
+                message.channel.send(`No songs to be added`)
+              }
+            })
+            .catch((err) => {
+              console.error(err)
+            })
+        })
+      }
+    } catch (err) {
+      console.error(err)
+      message.channel.send(`Error (playlist): ${err}`)
+    }
+  },
+}
